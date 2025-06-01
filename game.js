ed7635e2f2c22beafe1f61880d5849048f7df19d
MarySlaOq <mariaxavierrodrigues17@gmail.com>
2025-06-01 21:24:04 +0100

    sounds yippie!!

--- game.js
+++ game.js
@@ -6,20 +6,22 @@ document.addEventListener("DOMContentLoaded", () => {
     Game.game_Init();
     
 
-    // Add interactables
+    // Add interactables (lados esq/right, up/down, hitbox sides size, hitbox height size )
 
-    // Add trashcan (lados esq/right, up/down, hitbox sides size, hitbox height size )
-    let trashcan = new Interactable(700, 500, 130, 140, () => {
+    // Add trashcan 
+    let trashcan = new Interactable(995, 550, 200, 200, () => {
         window.open("countdown.html", '_self'); 
     });
     trashcan.addSprite("imgs/trash.png"); // Add sprite to the trashcan interactable
     
     Game.game_RegisterInteractable("Kitchen", trashcan);
     
+    // Add scenes  hitbox = lados esq/right, up/down, hitbox sides size, hitbox height size )
     
     // Opeanable fridge
     Game.game_RegisterInteractable("Fridge", new Interactable(500, 70, 320, 630, () => {
         console.log("Fridge opened");
+        
         Game.game_GotoScene("FridgeOpen");
     }));
     
@@ -30,14 +32,23 @@ document.addEventListener("DOMContentLoaded", () => {
     
     //openable cabinet
     
-    Game.game_RegisterInteractable("Cabinet", new Interactable(500, 100, 400, 630, () => {
+    Game.game_RegisterInteractable("Cabinet", new Interactable(800, 300, 300, 200, () => {
         console.log("Cabinet opened");
         Game.game_GotoScene("CabinetOpen");
     }));
     
-    Game.game_RegisterInteractable("CabinetOpen", new Interactable(500, 100, 400, 630, () => {
+    Game.game_RegisterInteractable("CabinetOpen", new Interactable(800, 300, 300, 200, () => {
         console.log("Cabinet closed");
         Game.game_GotoScene("Cabinet");
     }));
 
 });
+
+
+//sounds
+function playSound(fileName){
+    document.getElementById("shotGun").setAttribute('src', fileName);
+    document.getElementById("button").setAttribute('src', fileName);
+   // document.getElementById("shotGun").load(); //call this to just preload the audio without playing
+   // document.getElementById("birdsSound").play(); //call this to play the song right away
+}
